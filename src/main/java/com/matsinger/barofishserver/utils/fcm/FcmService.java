2d08a85ecbce0eafab7856de7ea4bc3ce5b74fb9
stk346 <stk346@gmail.com>
2025-03-04 04:49:51 +0900

    feat: 애플리케이션이 Sms 인프라스트럭처에 직접 의존하고 있는 문제 개선

--- src/main/java/com/matsinger/barofishserver/utils/fcm/FcmService.java
+++ src/main/java/com/matsinger/barofishserver/utils/fcm/FcmService.java
@@ -12,7 +12,6 @@ import java.util.List;
 @RequiredArgsConstructor
 @Service
 public class FcmService {
-    // private final FirebaseMessaging firebaseMessaging;
     private final FcmTokenRepository fcmTokenRepository;
     private final FcmConfig fcmConfig;
 
