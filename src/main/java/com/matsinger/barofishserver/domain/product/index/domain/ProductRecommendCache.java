465dc87dc56aae3267f41f70f2102c6946504cc4
stk346 <stk346@gmail.com>
2025-04-14 03:40:26 +0900

    refactor: ProductRecommendCache - id추가 및 기존 아이디 조합 해제

--- src/main/java/com/matsinger/barofishserver/domain/product/index/domain/ProductRecommendCache.java
+++ src/main/java/com/matsinger/barofishserver/domain/product/index/domain/ProductRecommendCache.java
@@ -1,24 +1,44 @@
 package com.matsinger.barofishserver.domain.product.index.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Entity
 @Table(name = "product_recommend_cache")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProductRecommendCache {
-    @EmbeddedId
-    private ProductRecommendCacheId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private Integer categoryId;
+
+    @Column
+    private Integer subCategoryId;
 
     @Column(nullable = false, columnDefinition = "MEDIUMTEXT")
     private String productIds;
 
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+
+    public static ProductRecommendCache from(Integer categoryId, Integer subCategoryId, String productIds) {
+        return ProductRecommendCache.builder()
+                .categoryId(categoryId)
+                .subCategoryId(subCategoryId)
+                .productIds(productIds)
+                .build();
+
+    }
 
     @PrePersist
     @PreUpdate
--- src/main/java/com/matsinger/barofishserver/domain/product/index/repository/ProductRecommendCacheRepository.java
+++ src/main/java/com/matsinger/barofishserver/domain/product/index/repository/ProductRecommendCacheRepository.java
@@ -1,10 +1,9 @@
 package com.matsinger.barofishserver.domain.product.index.repository;
 
 import com.matsinger.barofishserver.domain.product.index.domain.ProductRecommendCache;
-import com.matsinger.barofishserver.domain.product.index.domain.ProductRecommendCacheId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRecommendCacheRepository extends JpaRepository<ProductRecommendCache, ProductRecommendCacheId> {
+public interface ProductRecommendCacheRepository extends JpaRepository<ProductRecommendCache, Integer> {
 } 
