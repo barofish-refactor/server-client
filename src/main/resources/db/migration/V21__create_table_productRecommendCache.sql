9f8e6fd32b82bd10ba279f97521eb1e6db5d714e
stk346 <stk346@gmail.com>
2025-04-14 03:38:53 +0900

    flyway: V21__create_table_productRecommendCache.sql - 파일 삭제 -> 로컬에서는 플라이웨이를 사용하는게 번거로워 앞으로 사용하지 않을 예정

--- src/main/resources/db/migration/V21__create_table_productRecommendCache.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists product_recommend_cache (
-     category_id BIGINT NOT NULL,
-     sub_category_id BIGINT NOT NULL,
-     product_ids MEDIUMTEXT NOT NULL, -- 또는 MEDIUMTEXT
-     updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-     PRIMARY KEY (category_id, sub_category_id)
-);
