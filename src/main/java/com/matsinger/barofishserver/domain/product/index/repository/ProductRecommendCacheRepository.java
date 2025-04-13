c3d22206e176c98fbb0484735eb2ab999d98fc46
stk346 <stk346@gmail.com>
2025-04-14 07:22:28 +0900

    refactor: ProductRecommend.weight - float -> int로 타입 변경

--- src/main/java/com/matsinger/barofishserver/domain/product/index/repository/ProductRecommendCacheRepository.java
+++ src/main/java/com/matsinger/barofishserver/domain/product/index/repository/ProductRecommendCacheRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRecommendCacheRepository extends JpaRepository<ProductRecommendCache, Integer> {
-} 
+    boolean existsByCategoryIdAndSubCategoryId(Integer pCategoryId, Integer cCategoryId);
+}
