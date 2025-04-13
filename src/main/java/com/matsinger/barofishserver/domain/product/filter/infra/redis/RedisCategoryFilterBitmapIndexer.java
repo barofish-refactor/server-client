7c89d28d51242adfa561578e6ae614fdba5a7093
stk346 <stk346@gmail.com>
2025-04-14 01:13:32 +0900

    refactor: RedisCategoryFilterBitmapIndexer - 이름 변경

--- src/main/java/com/matsinger/barofishserver/domain/product/filter/infra/redis/RedisCategoryFilterBitmapIndexer.java
+++ src/main/java/com/matsinger/barofishserver/domain/product/filter/infra/redis/RedisCategoryFilterBitmapIndexer.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class RedisBitmapIndexer {
+public class RedisCategoryFilterBitmapIndexer {
     private final CategoryFilterProductsRepository categoryFilterProductsRepository;
     private final StringRedisTemplate redis;
 
